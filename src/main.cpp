1384f3e671c5055b4a7b36ab97b5ded6d863fd49
NickKipshidze <kipshidze.nick@gmail.com>
2023-08-31 10:38:15 +0400

    Add termination handling, version output, fixed memory leak

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,22 @@
 #include <iostream>
 #include <cstring>
-#include <string>
-#include <unistd.h>
 #include <pwd.h>
 #include <sys/wait.h>
 #include <vector>
 #include <sstream>
 
+#include "config.h"
+
 using namespace std;
 
 bool run = true;
+
+void handleTerm(int signum) {
+    if (signum == SIGINT) {
+        if (getpid() != -1)
+            kill(getpid(), SIGTERM);
+    }
+}
 
 string prompt(string prompt) {
     string command;
@@ -28,23 +35,56 @@ passwd *getuser() {
     return pwd;
 }
 
+string genpromptstr(string format) {
+    // %u - username
+    // %d - directory
+
+    string current_dir = getcwd(0, 0);
+    string home_dir = getuser()->pw_dir;
+
+    if (current_dir.substr(0, home_dir.size()) == home_dir)
+        current_dir.replace(0, home_dir.length(), "~");
+
+    for (int chr = 0; chr < (int)format.size(); chr++) {
+        if (format[chr] == '%') {
+            switch (format[chr+1]) {
+                case 'u':
+                    format.replace(chr, 2, getuser()->pw_name);
+                    break;
+
+                case 'd':
+                    format.replace(chr, 2, current_dir);
+                    break;
+                
+                default:
+                    break;
+            }
+        }
+    }
+
+    return format;
+}
+
 bool specialcom(char **argv) {
     if ((string)argv[0] == "exit") {
         run = false;
     } else if ((string)argv[0] == "cd") {
         string homedir = getuser()->pw_dir;
-
+        
         for (int arg = 0; argv[arg] != nullptr; arg++) {
-            string args = (string)argv[arg];
+            string argstr = (string)argv[arg];
 
-            for (int i = 0; i < (int)args.size(); i++){
-                if (args[i] == '~') {
-                    args.replace(i, 1, homedir);
+            for (int i = 0; i < (int)argstr.size(); i++){
+                if (argstr[i] == '~') {
+                    argstr.replace(i, 1, homedir);
                 }
             }
 
-            strcpy(argv[arg], (char *)args.c_str());
+            argv[arg] = strdup(argstr.c_str());
         }
+
+        if (argv[1] == nullptr)
+            argv[1] = strdup(homedir.c_str());
 
         chdir(argv[1]);
     } else {
@@ -64,22 +104,39 @@ char **parsecom(string command) {
     
     char** argv = new char*[args.size() + 1];
     for (size_t i = 0; i < args.size(); i++) {
-        argv[i] = const_cast<char*>(args[i].c_str());
+        argv[i] = new char[args[i].size() + 1];
+        strcpy(argv[i], args[i].c_str());
     }
     argv[args.size()] = nullptr;
 
     return argv;
 }
 
-int main() {
+int main(int argc, char *argv[]) {
     chdir(getuser()->pw_dir);
+    signal(SIGINT, SIG_IGN);
+
+    putenv((char *)"SHELL=vish");
+
+    for (int arg = 1; arg < argc; arg++) {
+        if (!strcmp(argv[arg], "--version")) {
+            cout << "VISH, version 1.0.0" << endl;
+            return 0;
+        }
+    }
+
+    string command;
+    char **args;
 
     while (run) {
-        string command = prompt((string)getcwd(0, 0) + (string)" > "); 
+        command = prompt(genpromptstr(propmtFormat));
+        
+        if (command.length() <= 0)
+            continue;
 
-        char **argv = parsecom(command);
+        args = parsecom(command);
 
-        if (!specialcom(argv)) {
+        if (!specialcom(args)) {
             run = false;
 
             pid_t pid = fork();
@@ -87,15 +144,21 @@ int main() {
             if (pid < 0) {
                 return 1;
             } else if (pid == 0) {
-                execvp(argv[0], argv);
+                signal(SIGINT, handleTerm);
+                exit(
+                    execvp(args[0], args)
+                );
             } else {
                 int childStatus;
                 waitpid(pid, &childStatus, 0);
+                if (childStatus == 65280)
+                    cerr << "vish: Unknown command: " << args[0] << endl;
                 run = true;
             }
         }
 
         command.clear();
+        free(args);
     }
 
     return 0;
